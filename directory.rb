cf4761f847bf41d66f893b6e16fe91113dbf253c
Amanda Vuong <mandyvuong@gmail.com>
2021-08-19 19:38:30 +0100

    ask for user input on cohort

--- directory.rb
+++ directory.rb
@@ -8,12 +8,42 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name}
     puts "Now we have #{students.count} students"
     # get another name from the user
     name = gets.chomp
   end
   # return the array of students
+  students
+end
+
+def input_cohort(students)
+  cohort_choice = {
+    1 => :January,
+    2 => :February,
+    3 => :March,
+    4 => :April,
+    5 => :May,
+    6 => :June,
+    7 => :July,
+    8 => :August,
+    9 => :September,
+    10 => :October,
+    11 => :November,
+    12 => :December
+    } 
+
+  students.each do |student|
+    puts "#{student[:name]}, please enter the cohort you are joining by entering the months number, i.e. 1 refers to January"
+    student_cohort = gets.chomp.to_i
+    if !cohort_choice[student_cohort].nil? 
+      student[:cohort] = cohort_choice[student_cohort]
+    else
+      student_cohort = cohort_choice[rand(1..12)]
+      puts "You did not enter a valid cohort, therefore we randomly selected you to join #{student_cohort}"
+      student[:cohort] = student_cohort
+    end
+  end
   students
 end
 
@@ -33,6 +63,7 @@ def print_footer(students)
 end
 
 students = input_students
+input_cohort(students)
 #nothing happens until we call the methods
 if students.count > 0
   print_header
